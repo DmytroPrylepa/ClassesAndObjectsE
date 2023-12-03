73115b2b3f78ee7b5ae071377f24b5b9e86048d1
DmytroPrylepa <prilepa.dm@gmail.com>
2023-12-03 21:23:20 +0200

    Update cars.java

--- src/classesandobjectse/cars.java
+++ src/classesandobjectse/cars.java
@@ -13,7 +13,17 @@ public class Cars {
     String model;
     String colour;
     int numWheels; 
+
+    public Cars(String manufacturer, String model, String colour, int numWheels) {
+        this.manufacturer = manufacturer;
+        this.model = model;
+        this.colour = colour;
+        this.numWheels = numWheels;
+    }
+    
+    
     public void Drive(String direction)   {
         System.out.println("The" + colour + " " + manufacturer + " is driving" + direction);
+        
 }
 }
